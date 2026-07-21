90811606431039cf769a76f83f46e48654f53434
hsonjecko-web <iraqtester993@gmail.com>
2026-07-22 00:45:46 +0300

    ملف index.html مدمج - يعمل مباشر + GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: './',
+  base: '',
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
-      manifest: {
-        name: 'العصر الذهبي - متجر الأجهزة الإلكترونية',
-        short_name: 'العصر الذهبي',
-        description: 'تسوق أجهزة إلكترونية بالقسط والحاضر - بالدينار العراقي',
-        theme_color: '#d4a843',
-        background_color: '#0a1628',
-        display: 'standalone',
-        orientation: 'portrait',
-        dir: 'rtl',
-        lang: 'ar',
-        start_url: './',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24
-              }
-            }
-          }
-        ]
-      }
-    })
+    viteSingleFile()
   ],
   resolve: {
     alias: {
